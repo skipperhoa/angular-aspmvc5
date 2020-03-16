7d078299aa07068558ac7d39f21cc6304cdb94c4
unknown <thanhhoacth2013@gmail.com>
2020-03-16 13:44:49 +0700

    Angular + ASP MVC 5

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Angular_mvc5
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+
+            bundles.Add(new ScriptBundle("~/Script/Bundles").Include(
+                     "~/dist/runtime-es2015.*",
+                      "~/dist/runtime-es5.*",
+                     "~/dist/polyfills-es2015.*",
+                      "~/dist/polyfills-es5.*",
+                     "~/dist/main-es2015.*",
+                      "~/dist/main-es5.*"));
+
+            bundles.Add(new StyleBundle("~/Content/Styles").Include(
+                      "~/dist/style.*"));
+        }
+    }
+}
